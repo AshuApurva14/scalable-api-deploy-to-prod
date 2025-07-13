84149aab69c15ab47c87e9bb7a230ef3a4596a37
Ashutosh Apurva <apooorva01@gmail.com>
2025-07-13 09:43:04 +0000

    feat:add cicd workflow for frontend services

--- scripts/aws_ecr_repo_creation.sh
+++ scripts/aws_ecr_repo_creation.sh
@@ -2,6 +2,14 @@
 
 set -e
 
-echo "Let's create a Private Image Registry in AWS ECR"
+echo "Let's create a Private Image Registry for Backend services in AWS ECR"
 
 aws ecr create-repository --repository-name node-api
+
+echo "Wait it is creating...!"
+
+
+echo "Let's create a Private Image Registry for Frontend services in AWS ECR"
+
+aws ecr create-repository --repository-name nginx-frontend
+
